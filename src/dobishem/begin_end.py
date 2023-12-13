d312bfaf28b7e8ddc343ce22a0ebe805e8a0fb29
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-12-13 11:42:58 +0100

    Corrections to begin/end

--- src/dobishem/begin_end.py
+++ src/dobishem/begin_end.py
@@ -4,14 +4,14 @@ class BeginAndEndMessages:
 
     """Run some code with begin and end/abandoned messages."""
 
-    def __init__(self, about, verbose):
+    def __init__(self, about, verbose=True):
         self.verbose = verbose
         self.about = about
 
     def __enter__(self):
         if self.verbose:
-            print("Beginning " + self.about)
+            print("Beginning", self.about)
 
-    def __enter__(self, exc_type, _exc_val, _exc_tb):
+    def __exit__(self, exc_type, _exc_val, _exc_tb):
         if self.verbose:
-            print(("Abandoned" if exc_type else "Finished ") + self.about)
+            print(("Abandoned" if exc_type else "Finished"), self.about)

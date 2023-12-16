28a1e6fee97572c196a36f97f690696bfc7c1cd5
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-12-16 20:17:52 +0100

    change how nesting works, and add timing

--- src/dobishem/nested_messages.py
+++ src/dobishem/nested_messages.py
@@ -1,27 +1,35 @@
 """Output begin and end messages."""
 
-class NestedMessages:
+import datetime
+
+class BeginAndEndMessages:
 
     """Run some code with nested begin and end/abandoned messages."""
 
-    depth = 0
+    prefixes = []
 
-    def __init__(self, about, margin="    ", verbose=True):
+    def __init__(self, about,
+                 margin="    ",
+                 verbose=True):
         self.verbose = verbose
         self.about = about
-        self.prefix = ""
+        self.margin = margin
+        self.started = None
 
     def __enter__(self):
-        self.depth += 1
-        self.prefix = self.margin * self.depth
         if self.verbose:
             print(self.prefix, "Beginning", self.about)
+        self.prefixes.append(self.margin)
+        self.prefix = "".join(self.prefixes)
+        self.started = datetime.datetime.now()
         return self
 
     def print(self, text):
         print(self.prefix, text)
 
     def __exit__(self, exc_type, _exc_val, _exc_tb):
+        time_taken = datetime.datetime.now() - self.started
+        self.prefixes.pop()
+        self.prefix = "".join(self.prefixes)
         if self.verbose:
-            print(self.prefix, ("Abandoned" if exc_type else "Finished"), self.about)
-        self.depth -= 1
+            print(self.prefix, ("Abandoned" if exc_type else "Finished"), self.about, "in", time_taken)

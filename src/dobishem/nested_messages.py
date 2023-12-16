762f7a5b857889e02337526d92302baa2987fcfc
John C. G. Sturdy <jcg.sturdy@gmail.com>
2023-12-16 21:15:28 +0100

    fix nested messages

--- src/dobishem/nested_messages.py
+++ src/dobishem/nested_messages.py
@@ -2,11 +2,11 @@
 
 import datetime
 
+prefixes = list()
+
 class BeginAndEndMessages:
 
     """Run some code with nested begin and end/abandoned messages."""
-
-    prefixes = []
 
     def __init__(self, about,
                  margin="    ",
@@ -15,23 +15,33 @@ class BeginAndEndMessages:
         self.about = about
         self.margin = margin
         self.started = None
-        self.prefix = ""
-        self.prefixes = []
+        self.prefix = "".join(prefixes)
 
     def __enter__(self):
         if self.verbose:
-            print(self.prefix, "Beginning", self.about)
-        self.prefixes.append(self.margin)
-        self.prefix = "".join(self.prefixes)
+            print(self.prefix + "Beginning", self.about)
+        prefixes.append(self.margin)
+        self.prefix = "".join(prefixes)
         self.started = datetime.datetime.now()
         return self
 
     def print(self, text):
-        print(self.prefix, text)
+        print(self.prefix + text)
 
     def __exit__(self, exc_type, _exc_val, _exc_tb):
         time_taken = datetime.datetime.now() - self.started
-        self.prefixes.pop()
-        self.prefix = "".join(self.prefixes)
+        prefixes.pop()
+        self.prefix = "".join(prefixes)
         if self.verbose:
-            print(self.prefix, ("Abandoned" if exc_type else "Finished"), self.about, "in", time_taken)
+            print(self.prefix + ("Abandoned" if exc_type else "Finished"), self.about, "in", time_taken)
+
+# if __name__ == "__main__":
+#     with BeginAndEndMessages("outer one"):
+#         with BeginAndEndMessages("middle one") as mid:
+#             mid.print("inside mid one")
+#             with BeginAndEndMessages("inner") as inner:
+#                 inner.print("inner")
+#         with BeginAndEndMessages("middle two") as mid:
+#             mid.print("inside mid two")
+#         with BeginAndEndMessages("middle three") as mid:
+#             mid.print("inside mid three")
